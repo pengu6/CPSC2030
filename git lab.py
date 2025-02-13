d804363585e0e50192693c011bd47d0e389ee67a
Antonio <av135791@gmail.com>
2025-02-13 10:05:26 -0500

    Check winnner function finished

--- git lab.py
+++ git lab.py
@@ -4,4 +4,11 @@ def print_board(board):
         print('-'*9)
 
 def check_winner(board , player):
-    pass
+    for i in range(3):
+        if all([board[i][j] == player for j in range(3)]) or \
+           all([board[j][i] == player for j in range(3)]):
+            return True
+    if all([board[i][i] == player for i in range(3)]) or \
+    all([board[i][2 - i] == player for i in range(3)]):
+        return True
+    return False
