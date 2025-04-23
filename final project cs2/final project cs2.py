1104b7d789cfb557cfae04e72324d5dcb6ffc84b
Antonio Vega <antiman324@gmail.com>
2025-04-23 18:53:52 -0400

    Added where you can get a library card, and added the main class that calls the rest of the classes.

--- final project cs2/final project cs2.py
+++ final project cs2/final project cs2.py
@@ -41,3 +41,45 @@ class Librarian(Person):
 
     def get_role(self) -> str:
         return "Librarian"
+
+
+class Book:
+    def __init__(self, title: str, isbn: str, author: Author):
+        self._title = title
+        self._isbn = isbn
+        self._author = author
+
+class EBook(Book):
+    def __init__(self, title: str, isbn: str, author: Author, file_format: str):
+        super().__init__(title, isbn, author)
+        self._file_format = file_format
+
+class PrintedBook(Book):
+    def __init__(self, title: str, isbn: str, author: Author, pages: int):
+        super().__init__(title, isbn, author)
+        self._pages = pages
+
+
+class Library:
+    def __init__(self, name: str):
+        self._name = name
+        self._books: List[Book] = []
+        self._members: List[Member] = []
+        self._librarians: List[Librarian] = []
+
+    def add_book(self, book: Book):
+        self._books.append(book)
+
+    def add_member(self, member: Member):
+        self._members.append(member)
+
+    def add_librarian(self, librarian: Librarian):
+        self._librarians.append(librarian)
+
+    def get_books(self) -> List[Book]:
+        return self._books
+
+class LibraryCard:
+    def __init__(self, card_number: int, member: Member):
+        self._card_number = card_number
+        self._member = member
